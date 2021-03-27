6a91ded3498e14c085ed160e98b24cad2e2f6e22
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-27 18:56:13 -0500

    Implement subscribe_returns_400 integrations test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -54,3 +54,35 @@ async fn subscribe_returns_200_for_valid_form_data() {
     // Assert
     assert_eq!(200, response.status().as_u16());
 }
+
+#[actix_rt::test]
+async fn subcribe_returns_400_when_data_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing email"),
+        ("email=ursula_le_guin%40gmail.com", "missing name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
