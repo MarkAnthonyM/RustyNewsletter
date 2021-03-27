a11021552022c4a5707ed6aee9d342fb17186b00
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-27 17:24:45 -0500

    Refactor health_check test to randomize ports

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,14 @@
+use std::net::TcpListener;
+
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -18,11 +19,19 @@ async fn health_check_works() {
 }
 
 // Launch our application in the background
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // Bind to address
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    // Retrieve port assgined by OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
 
     // Launch server as a background task
     // tokio::spawn returns a handle to spawned future,
     // but we have no use for it here
     let _ = tokio::spawn(server);
+
+    // Return address to caller
+    format!("http://127.0.0.1:{}", port)
 }
