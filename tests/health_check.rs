1d14372a7d4e846698b78aaf0e3500484d2743f1
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-27 07:59:05 -0500

    Implement health_check_works integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,28 @@
-use zero2prod::main;
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app();
 
-#[test]
-fn dummy_test() {
-    main()
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    // Launch server as a background task
+    // tokio::spawn returns a handle to spawned future,
+    // but we have no use for it here
+    let _ = tokio::spawn(server);
 }
