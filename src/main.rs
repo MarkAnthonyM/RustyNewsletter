af95eb4946c6442d3867658ec1d3fdbf4c46a465
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-27 06:21:22 -0500

    Remove actix_web hello world code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use actix_web::{ web, App, HttpRequest, HttpResponse, HttpServer, Responder };
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
@@ -13,8 +8,6 @@ async fn health_check() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
     })
     .bind(("127.0.0.1", 8080))?
