8bb9a28d27aacffa1ce403add21ecad8b9646d8f
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-31 03:26:42 -0500

    Add db data connection to App state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::net::TcpListener;
-use zero2prod::{configuration::get_configuration, startup::run};
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,19 @@
 use super::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
+use std::sync::Arc;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap db connection in an Arc for clonability
+    let connection = Arc::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register pointer copy to db connection as part of the application state
+            .data(connection.clone())
     })
     .listen(listener)?
     .run();
