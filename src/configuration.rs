65319572752d03b9a3efb598ac1733fd220b0e47
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-30 20:43:45 -0500

    Update configuration file path

--- src/configuration.rs
+++ src/configuration.rs
@@ -31,7 +31,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Add configuration values from a file named 'configuration'.
     // It will look for any top-level file with an extension
     // that 'config' knows how to parse: yaml, json, etc.
-    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
+    settings.merge(config::File::from(configuration_directory.join("personal")).required(true))?;
 
     // Try to convert the configuration values it read into
     // or Settings type
