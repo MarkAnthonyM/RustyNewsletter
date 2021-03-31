20491d2661e0330aea442973aca65c692958cad3
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-03-31 15:08:05 -0500

    Update app logic to make use of connection pooling

--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,18 @@
 use super::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
-use std::sync::Arc;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap db connection in an Arc for clonability
-    let connection = Arc::new(connection);
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Register pointer copy to db connection as part of the application state
-            .data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
