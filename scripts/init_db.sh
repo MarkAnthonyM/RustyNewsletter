84e3c2a5ed52266723bff53223424cee729895ef
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-04-01 22:53:29 -0500

    Update port default in init_db

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -7,7 +7,7 @@ DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
+DB_PORT="${POSTGRES_PORT:=6543}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
