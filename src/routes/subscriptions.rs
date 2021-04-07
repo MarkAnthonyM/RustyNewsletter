a9895b9c6799489bfcffa059060d731d78d93630
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-04-07 06:03:50 -0500

    Move database query logic to seprate function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -23,7 +23,21 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+    insert_subscriber(&pool, &form)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -34,13 +48,12 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
-    .instrument(query_span)
+    .execute(pool)
     .await
     .map_err(|e| {
         tracing::error!("Failed to execute query: {:?}", e);
-        HttpResponse::InternalServerError().finish()
+        e
     })?;
 
-    Ok(HttpResponse::Ok().finish())
+    Ok(())
 }
