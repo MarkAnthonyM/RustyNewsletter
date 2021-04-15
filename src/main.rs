93fa6b25d3447ed6f68819cce3a17b685e569136
MarkAnthonyM <MarkAnthonyMartinez9@gmail.com>
2021-04-15 00:39:37 -0500

    Adjust Postgres connection timeout length

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -15,10 +15,16 @@ async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_string = configuration.database.connection_string();
-    // let connection_pool = PgPool::connect(&connection_string)
     // Bug: Something up with sqlx right now. Need to use connect_lazy for now
-    let connection_pool =
-        PgPool::connect_lazy(&connection_string).expect("Failed to connect to postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    // let connection_pool = PgPoolOptions::new()
+    //     .connect_timeout(std::time::Duration::from_secs(2))
+    //     .connect_lazy(&connection_string)
+    //     .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
